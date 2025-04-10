4258cb58d48fe7229f8aeaa9878fd9f5bb35bdc5
Arthur Avila <arthurpro295@gmail.com>
2025-04-10 13:19:08 -0300

    Corrigindo erro

--- src/app/api/lembrancas/[id]/route.ts
+++ src/app/api/lembrancas/[id]/route.ts
@@ -1,61 +1,72 @@
-import { createClient } from '@supabase/supabase-js'
-
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest } from 'next/server';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
 
-
-export async function GET( request: Request,
-    { params }: { params: { id: string } }) {
-
-    const id = params.id;
-
-   const{ data, error } = await supabase.from('lembrancas').select('*').eq('id', id).single(); 
-
-  if (!id) {
-  return new Response(JSON.stringify({ error: 'ID não fornecido' }), {
-    status: 400,
-    headers: { 'Content-Type': 'application/json' },
-  });
-}
-  
-  if (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
-  }
-
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
-
-
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  const id = params.id
+// GET /api/lembrancas/[id]
+export async function GET(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
 
   if (!id) {
     return new Response(JSON.stringify({ error: 'ID não fornecido' }), {
       status: 400,
       headers: { 'Content-Type': 'application/json' },
-    })
+    });
   }
 
-  const { error } = await supabase.from('lembrancas').delete().eq('id', Number(id))
+  const { data, error } = await supabase
+    .from('lembrancas')
+    .select('*')
+    .eq('id', id)
+    .single();
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
       headers: { 'Content-Type': 'application/json' },
-    })
+    });
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+// DELETE /api/lembrancas/[id]
+export async function DELETE(
+  request: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = context.params.id;
+
+  if (!id) {
+    return new Response(JSON.stringify({ error: 'ID não fornecido' }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  const { error } = await supabase
+    .from('lembrancas')
+    .delete()
+    .eq('id', id);
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
   }
 
   return new Response(JSON.stringify({ message: 'Deletado com sucesso' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
-  })
+  });
 }
